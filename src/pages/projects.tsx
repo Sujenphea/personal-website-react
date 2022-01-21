fed6a2889ecbb937a39ad4630ec0cbc7ab5d5dc8
sujen <sujenphea@gmail.com>
2022-01-22 06:03:31 +1300

    feature: add project cards into projects
    - implement flex flow projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -3,32 +3,45 @@
 import { css, jsx } from "@emotion/react";
 
 const Projects = () => {
+  const testArray = [1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4];
+
   return (
     <div className="category-container">
       {/* title */}
       <div className="title">Projects</div>
       {/* contents centered relative to container div */}
       <div
+        id="projects-container"
         style={{
           display: "flex",
           flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-evenly",
           width: "100vw",
+          overflow: "scroll",
         }}
       >
-        center
-        <div
-          style={{
-            width: "200px",
-            height: "200px",
-            border: "1px solid #eee",
-          }}
-        >
-          sample
-        </div>
-        hello
+        {testArray.map((d) => {
+          return <ProjectCard />;
+        })}
       </div>
+    </div>
+  );
+};
+
+const ProjectCard = () => {
+  return (
+    <div
+      style={{
+        width: "250px",
+        height: "400px",
+        border: "1px solid #eee",
+        marginLeft: "20px",
+        marginRight: "20px",
+        marginTop: "20px",
+        marginBottom: "20px",
+        flexShrink: 0,
+      }}
+    >
+      sample
     </div>
   );
 };
